83eff8a6741fc8cd761ec67b2d0c9a26b70fd402
Szymon Bobrowski <szymekbobrowski@wp.pl>
2022-12-10 12:55:52 +0100

    Add sample graph for Johnson algorithm

--- data/graphs_example_data.py
+++ data/graphs_example_data.py
@@ -1,5 +1,5 @@
 def main():
-    vertices, edges = get_example_3()
+    vertices, edges = get_example_4()
     return vertices, edges
 
 def get_example_1():
@@ -15,4 +15,10 @@ def get_example_2():
 def get_example_3():
     vertices = ['1', '2', '3', '4']
     edges = [['1', '2', 3], ['1', '4', 7], ['2', '1', 8], ['2', '3', 2], ['3', '1', 5], ['3', '4', 1], ['4', '1', 2]]
+    return vertices, edges
+
+def get_example_4():
+    vertices = ['a', 'b', 'c', 'd']
+    edges = [['a', 'b', -3], ['a', 'd', 2], ['b', 'a', 5], ['b', 'c', 3], ['c', 'a', 1], \
+         ['d', 'c', 4], ['d', 'a', -1]]
     return vertices, edges
